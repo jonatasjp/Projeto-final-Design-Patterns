4e7b8bc9dc56b3215862ffea54a8c824f64598e5
Jonatas Luis <jonatasluis.unipe@gmail.com>
2017-11-13 01:18:36 -0200

    teste adicionando topico ok

--- src/teste/TestComponenteGamificacao.java
+++ src/teste/TestComponenteGamificacao.java
@@ -1,10 +1,32 @@
 package teste;
 
+import java.util.Arrays;
 
+import org.junit.Test;
+
+import implementacao.AchievementObserverInventor;
+import implementacao.AchievementObserverPartOfTheCommunity;
+import implementacao.AchievementStorageFactory;
+import implementacao.ForumService;
+import implementacao.ForumServiceGamificationProxy;
+import implementacao.ForumServiceMock;
+import implementacao.MemoryAchievementStorage;
 
 public class TestComponenteGamificacao {
 
-
+	@Test
+	public void testeAdicionandoTopico() {
+		
+		MemoryAchievementStorage storage = MemoryAchievementStorage.criarMemoryAchievementStorage();
+		storage.setObservadores(Arrays.asList(new AchievementObserverInventor(), new AchievementObserverPartOfTheCommunity()));
+		
+		AchievementStorageFactory.setAchievementStorage(storage);
+		
+		ForumService forumService = new ForumServiceGamificationProxy(new ForumServiceMock());
+		
+		forumService.addTopic("jonatas", "Topico de teste1");
+		
+	}
 
 
 	
